5d184e91f7c13f7d59892c90a4799f32470b0504
Kevin Hernon <khernon1@gmail.com>
2016-10-20 17:02:16 -0400

    two edge cases done

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # OscarsAPI
+
+done so as to limit the number of iterations that were needed
+
+budget cases
+- says 'millions' ie $10 million - done
+- has actual value ie $10,000,000 - done
+- ranges need to get average
+- in GBP
+
+needs to take out all integers plus the .
--- main/challenge.py
+++ main/challenge.py
@@ -13,45 +13,67 @@ class OscarsAPI:
 
   def populate_winning_films(self):
     films_per_year = self.all_films['results']
+    # average_budget = 0
+    # film_count = 0
     for films in films_per_year:
-        # removes footnotes and whitespace
-        cleaned_year = re.sub("[\(\[].*?[\)\]]", '', films.values()[1]).strip()
-      
+      # removes footnotes and whitespace
+      cleaned_year = re.sub("[\(\[].*?[\)\]]", '', films.values()[1]).strip()
+    
+
       for film in films.values()[0]:
         if film['Winner'] == True:
           # removes footnotes and whitespace
           cleaned_title = re.sub("[\(\[].*?[\)\]]", '', film['Film']).strip()
-          self.winning_films.append({"name": cleaned_title, "detail_url": film['Detail URL'], "year": cleaned_year})
-
+          added_film = {"name": cleaned_title, "detail_url": film['Detail URL'], "year": cleaned_year}
+          self.winning_films.append(added_film)
+          
+          self.add_budget_to_winning_films(added_film)
+          self.format_budget_number(added_film)
+          self.print_winning_films(added_film)
+          
+    #       if added_film['budget'] != 0:
+    #         average_budget += added_film['budget']
+    #         film_count += 1
+          
+    # print "Average - %d" % int(average_budget)
     
-  def add_budget_to_winning_films(self):
-    for film in self.winning_films:
-      detail_url = film['detail_url']
-      response = urlopen(detail_url)
-      film_detail = json.load(response)  
+  def add_budget_to_winning_films(self, film):
+    detail_url = film['detail_url']
+    response = urlopen(detail_url)
+    film_detail = json.load(response)  
 
-      # some films don't have budget data
-      if 'Budget' in film_detail:
-        film['budget'] = film_detail['Budget']
-        film['budget'] = re.sub("[\(\[].*?[\)\]]", '', film['budget']).strip()
-      else:
-        film['budget'] = '0'
+    # some films don't have budget data
+    if 'Budget' in film_detail:
+      film['budget'] = film_detail['Budget']
+      film['budget'] = re.sub("[\(\[].*?[\)\]]", '', film['budget']).strip()
+    else:
+      film['budget'] = '0'
 
-      # add formulas to fix the budget
-      usd = re.findall(r'\$', film['budget'])
-      pdb.set_trace()
+  def format_budget_number(self, film):
+    # the code directly below the case numbers 
+    # deals with the individual edge cases    
+    million = re.findall(r'\million', film['budget'])
+    usd = re.findall(r'\$', film['budget'])
+    budget_number = re.findall(r'[\d.]*\d+', film['budget'])
+    budget = float(''.join(budget_number))
+
+    # case1 = majority are in USD format ie "$10 million" 
+    # or "$10.2 million" (budget variable is converted to float)
+    if usd and million:            
+      budget *= 1000000
 
 
-  def print_winning_films(self):
-    for film in self.winning_films:
-      print film['name'] + ' - ' + film['year'] + ' - ' + film['budget']
+    # case2 = actual value is given no change is needed ie "$1,430,000"
+    film['budget'] = budget    
+      
 
+  def print_winning_films(self, film):    
+    print film['name'] + ' - ' + film['year'] + ' - {:,d}'.format(int(round(film['budget'],0)))
+    
 
 
   def run_all(self):
     self.populate_winning_films()
-    self.add_budget_to_winning_films()
-    self.print_winning_films()
 
 
 go = OscarsAPI()
