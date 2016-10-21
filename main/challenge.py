44eb134caf6752ff56651837fcb4b916de8fef91
Kevin Hernon <khernon1@gmail.com>
2016-10-20 20:30:03 -0400

    last edge case with or

--- main/challenge.py
+++ main/challenge.py
@@ -2,7 +2,6 @@
 from urllib2 import urlopen
 import json
 import re
-import unicodedata
 import pdb
 
 class OscarsAPI:
@@ -15,8 +14,8 @@ class OscarsAPI:
 
   def populate_winning_films(self):
     films_per_year = self.all_films['results']
-    # average_budget = 0
-    # film_count = 0
+    total_budgets = 0
+    film_count = 0
     for films in films_per_year:
       # removes footnotes and whitespace
       cleaned_year = re.sub("[\(\[].*?[\)\]]", '', films.values()[1]).strip()
@@ -33,11 +32,12 @@ class OscarsAPI:
           self.format_budget_number(added_film)
           self.print_winning_films(added_film)
           
-    #       if added_film['budget'] != 0:
-    #         average_budget += added_film['budget']
-    #         film_count += 1
+          if added_film['budget'] != 0:
+            total_budgets += added_film['budget']
+            film_count += 1
+            average_budget = total_budgets/film_count
           
-    # print "Average - %d" % int(average_budget)
+    print "Average - %d" % int(average_budget)
     
   def add_budget_to_winning_films(self, film):
     detail_url = film['detail_url']
@@ -55,7 +55,10 @@ class OscarsAPI:
     # the code directly below the case number comments 
     # deals with the individual edge cases
     budget_range = re.findall(r'\-', film['budget'])
+    two_budgets = re.findall(r'\or', film['budget'])
 
+    if two_budgets:
+      self.two_budgets_given(film)
     # case1 = if a range is given ie "$10 - $12 million"
     if budget_range:
       self.budget_given_as_range(film)
@@ -72,23 +75,19 @@ class OscarsAPI:
       self.budget_not_in_usd(film, budget)
       budget_number = re.findall(r'[\d.]*\d+', film['budget'])
       budget = float(''.join(budget_number))
-#change to else if
-    if usd and million:            
+
+    if million:            
       budget *= 1000000
+    
     # case3 = actual value is given no change is needed ie "$1,430,000"
+    # otherwise if there was a change to the budget above it will update here
     film['budget'] = budget
 
 
-
-  def budget_not_in_usd(self, film, budget):
-    currency = film['budget'][0].encode('utf-8')
-    if currency == "£":
-      converted_budget = float(budget * 1.23)
-      film['budget'] = re.sub(r'\d.+', str(converted_budget), film['budget'])
-      pdb.set_trace()
-
+  def two_budgets_given(self, film):
+    split_budgets = film['budget'].split(' or')
+    film['budget'] = split_budgets[0]
       
-
   def budget_given_as_range(self, film):
     budget_number = re.findall(r'[\d.]*\d+', film['budget'])
     budget1 = float(''.join(budget_number[0]))
@@ -96,6 +95,12 @@ class OscarsAPI:
     average_budget_range = (budget1 + budget2)/2
     film['budget'] = re.sub(r'[\d.-]*\d+', str(average_budget_range), film['budget'])
       
+  def budget_not_in_usd(self, film, budget):
+    currency = film['budget'][0].encode('utf-8')
+    # add dict to search through and get rate
+    if currency == "£":
+      converted_budget = float(budget * 1.33)
+      film['budget'] = re.sub(r'\d.+', str(converted_budget), film['budget'])
 
   def print_winning_films(self, film):    
     print film['name'] + ' - ' + film['year'] + ' - {:,d}'.format(int(round(film['budget'],0)))
